daefd6b02eaf7954edc9940d1a0cfafa2533d0eb
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2024-11-06 12:33:12 +0100

    Initial Commit

---
+++ MoValidator/MoValidator.h
@@ -0,0 +1,18 @@
+//
+//  MoValidator.h
+//  MoValidator
+//
+//  Created by Mohamad Meksasi on 06/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoValidator.
+FOUNDATION_EXPORT double MoValidatorVersionNumber;
+
+//! Project version string for MoValidator.
+FOUNDATION_EXPORT const unsigned char MoValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoValidator/PublicHeader.h>
+
+
